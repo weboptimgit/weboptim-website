5c75940513c901cda84cbdf984ceea098fd2327e
weboptimgit <gaborik@weboptim.eu>
2025-12-13 20:45:06 +0100

    Update SEOServices.tsx

--- src/pages/SEOServices.tsx
+++ src/pages/SEOServices.tsx
@@ -24,6 +24,7 @@ import ServiceFAQ from "@/components/ServiceFAQ";
 import SEO from "@/components/SEO";
 import { useSeoLang } from "@/contexts/LanguageSEO";
 import { buildPath } from "@/config/domains";
+import { useLanguage } from "@/contexts/LanguageContext";
 
 // vizuálne farby nechávame v page (nemá sa prekladať)
 const seoServicesColors = [
