18c5873bd26c5363ba1f59a3d22372ad4c783d7d
weboptimgit <gaborik@weboptim.eu>
2025-12-15 01:25:53 +0100

    Update use-mobile.tsx

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,22 +3,25 @@ import * as React from "react";
 const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
+  const getIsMobile = () =>
+    typeof window !== "undefined"
+      ? window.innerWidth < MOBILE_BREAKPOINT
+      : false;
 
-  React.useEffect(() => {
+  const [isMobile, setIsMobile] = React.useState<boolean>(getIsMobile);
+
+  React.useLayoutEffect(() => {
     const mql = window.matchMedia(
       `(max-width: ${MOBILE_BREAKPOINT - 1}px)`
     );
 
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    };
+    const onChange = () => setIsMobile(mql.matches);
 
+    onChange();
     mql.addEventListener("change", onChange);
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
 
     return () => mql.removeEventListener("change", onChange);
   }, []);
 
-  return !!isMobile;
+  return isMobile;
 }
