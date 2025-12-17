6960fde4bef27383a97cba038a55465c146d73ca
weboptimgit <gaborik@weboptim.eu>
2025-12-17 18:27:15 +0100

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { Home } from "lucide-react";
 import AnimatedMascot from "@/components/AnimatedMascot";
-import mascotSleeping from "@/assets/mascot-sleeping.png";
+import mascotSleeping from "@/assets/mascot-sleeping-transparent.png";
 import SEO from "@/components/SEO";
 
 const NotFound = () => {
